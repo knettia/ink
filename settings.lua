7724137f88be17cdf9cd2ee60e47f173fb019f0e
Amelia Gäßler <146373382+knettia@users.noreply.github.com>
2024-01-24 09:07:42 +0000

    added settings wiki base

---
+++ settings.lua
@@ -0,0 +1,10 @@
+-- chat:
+["ChatEnabled"] = true, -- used to enable disable chat
+["BubbleChatEnabled"] = true, -- used to enable disable bubble chat, depended on ChatEnabled, but can be disabled if chat is enabled
+["ChatHorizontalSize"] = 1, -- how big the chat is, horizontally
+
+-- chat messages:
+["ChatMessageDisplayCount"] = 15, -- how many messages the chat displays
+["ChatMessageDestroyDelay"] = 20, -- how many seconds until a message gets destroyed, in seconds
+
+[""] = nil,

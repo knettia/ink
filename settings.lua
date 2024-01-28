2f569ee1ccc50a09744ffd73895429221336bf98
Amelia Gäßler <146373382+knettia@users.noreply.github.com>
2024-01-28 20:25:00 +0200

    updated for chat anchored

--- settings.lua
+++ settings.lua
@@ -1,7 +1,8 @@
 -- chat:
 ["ChatEnabled"] = true, -- used to enable disable chat
 ["BubbleChatEnabled"] = true, -- used to enable disable bubble chat, depended on ChatEnabled, but can be disabled if chat is enabled
-["ChatHorizontalSize"] = 1, -- how big the chat is, horizontally
+["ChatAnchoredPosition"] = 1, -- where the chat is anchored.
+["ChatHorizontalSize"] = .3, -- how big the chat is, horizontally
 
 -- chat messages:
 ["ChatMessageDisplayCount"] = 15, -- how many messages the chat displays

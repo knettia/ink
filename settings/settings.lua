7b8ba1ca756b111f00521c66a925efc2b9b3d5ed
Amelia Gäßler <146373382+knettia@users.noreply.github.com>
2024-02-01 23:04:53 +0200

    rewritten, updated comments

--- settings/settings.lua
+++ settings/settings.lua
@@ -1,11 +1,11 @@
--- chat:
-["ChatEnabled"] = true, -- used to enable disable chat
-["BubbleChatEnabled"] = true, -- used to enable disable bubble chat, depended on ChatEnabled, but can be disabled if chat is enabled
-["ChatAnchoredPosition"] = 1, -- where the chat is anchored.
-["ChatHorizontalSize"] = .3, -- how big the chat is, horizontally
+-- Chat:
+["ChatEnabled"] = true, -- Used to enable/disable chat
+["BubbleChatEnabled"] = true, -- Whether or not bubble chat is enabled (depends on ChatEnabled, but can be disabled if chat is enabled)
+["ChatAnchoredPosition"] = 1, -- Where the chat is anchored (1 for top, 0 for bottom)
+["ChatHorizontalSize"] = 0.3, -- How big the chat is, horizontally
 
--- chat messages:
-["ChatMessageDisplayCount"] = 15, -- how many messages the chat displays
-["ChatMessageDestroyDelay"] = 20, -- how many seconds until a message gets destroyed, in seconds
+-- Chat messages:
+["ChatMessageDisplayCount"] = 15, -- How many messages the chat displays (will probably be replaced with chatDisplayLineCount)
+["ChatMessageDestroyDelay"] = 20, -- How long until a message gets destroyed, in seconds
 
-[""] = nil,
+[""] = nil, -- Empty entry
